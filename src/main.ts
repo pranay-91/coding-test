fc0e939ec5cc282ad908eb0e880552c7863ab4f0
Pranay Pradhananga <pranay.pradhananga@team.telstra.com>
2021-05-20 16:07:52 +1000

    Initial release

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import app from './app';
 import config from './config';
 
 app.listen(config.app.port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Application listening on: ${config.app.port}`);
 });
