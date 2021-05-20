c51bf4899816c8cb7dab2aa83f1f317a59b00ab0
Pranay Pradhananga <pranay.pradhananga@team.telstra.com>
2021-05-20 17:55:07 +1000

    Add healthcheck endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,15 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import transformRouter from './transform';
+import HttpStatus from 'http-status-codes';
 
 const routes = Router();
+
+// Endpoint for health check.
+routes.get('/healthz', (req: Request, res: Response) => {
+  res.status(HttpStatus.OK).json();
+});
+
+// using transform Router
 routes.use('/transform', transformRouter);
 
 export default routes;
--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -6,6 +6,13 @@ import { goodData, goodTransformedPayload } from '../test/fixtures';
 import transformRecursive from '../helper/transform';
 
 describe('functional - transform', () => {
+  describe('Health check', () => {
+    it('should return 200 status OK on health check endpoint', async () => {
+      const res = await request(app).get('/healthz');
+      expect(res.status).to.equal(HttpStatus.OK);
+    });
+  });
+
   describe('Helper', () => {
     it('should transform given payload by replacing values with reference data.', () => {
       const transformPayload = { ...goodData.payload };
@@ -14,8 +21,8 @@ describe('functional - transform', () => {
     });
   });
 
-  describe('POST', () => {
-    it('should return transformed payload.', async () => {
+  describe('Routes', () => {
+    it('POST - should return transformed payload.', async () => {
       const res = await request(app).post('/transform').send(goodData);
       expect(res.status).to.equal(HttpStatus.OK);
       expect(res.body).to.deep.equal(goodTransformedPayload);
